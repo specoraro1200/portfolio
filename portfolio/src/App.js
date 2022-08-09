19e8650109a03135284c8b5a1c3ccbcc779f4c75
specoraro1200 <specoraro@luc.edu>
2022-08-09 16:01:13 -0500

    IDEK

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -60,7 +60,7 @@ function App() {
   {/* <!-- Custom fonts for this template --> */}
 
   <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,600,700,800,900" rel="stylesheet" />
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Raleway:ital,wght@0,100;0,200;0,210;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
   <link href="vendor/devicons/css/devicons.min.css" rel="stylesheet" />
   <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" />
